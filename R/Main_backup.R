7970f7b6616a7302ed7cd679ee6647077de09200
simoneb <simoneugomaria.bregaglio@crea.gov.it>
2025-02-03 15:34:44 +0100

    try to fix run

--- R/Main_backup.R
+++ R/Main_backup.R
@@ -160,7 +160,7 @@ swellCalibration <- function(weather_data, vegetation_data,
   }))
 
   # Write the table to file
-  write.table(SWELLparameters_df[,-1], file = parameters_file,
+  write.table(SWELLparameters_df, file = parameters_file,
               sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 
